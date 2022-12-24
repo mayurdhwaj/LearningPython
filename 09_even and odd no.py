e993ca8f1a15166abb3ced1f36c1de53e78a49f7
Mayurdhwaj <mayurdhwajs94@gmail.com>
2022-12-25 03:01:14 +0530

    Count even and odd number in given range

---
+++ 09_even and odd no.py
@@ -0,0 +1,10 @@
+a=int(input())
+count=0
+Count=0
+for i in range(1,a+1):
+    if i%2==0:
+        count+=1
+    else:
+        Count+=1
+print('Total Even no :',count)
+print('total odd no:',Count)
