a5f84d0b3e8cf0859fc1f777c6a91f9c43b37ef0
Mayurdhwaj <mayurdhwajs94@gmail.com>
2022-12-31 00:54:56 +0530

    renamed

--- 14.py
+++
@@ -1,8 +0,0 @@
-a=int(input('enter the roll no:'))
-rollno = [1,2,3,4,5,6,7,8]
-def is_present(a):
-    if a in rollno:
-        print(a,'roll no is present')
-    else:
-        print('absent')
-is_present(a)
--- 16.py
+++
@@ -1,5 +0,0 @@
-a=int(input('enter the radius:'))
-def area(a):
-    x=22.7*a*a
-    print('Area is:',x)
-area(a)
